fd8d5aa1b4847d256053fa6a941ed65372174e1c
Imberton, Guilhem <guilhem.imberton@intel.com>
2014-03-04 04:17:04 -0800

    [SDK] Removing eng debug and user flags from components makefiles
    BZ: 143553
    
    Change-Id: Ibaff524ceb7936829f477a03d5f166870894afcd
    Signed-off-by: Guilhem IMBERTON <guilhem.imberton@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,18 +1,15 @@
-LOCAL_PATH:=$(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_PRELINK_MODULE := false
-LOCAL_SRC_FILES:= \
-	intel_scu_ipcutil_test.c \
-	mem_dump.c \
-	gen_utils.c \
-	peeknpoke.c \
-	msr.c       \
-	i2c.c	    \
-	port.c
-
 LOCAL_MODULE := peeknpoke
-LOCAL_MODULE_TAGS := eng debug
-
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRELINK_MODULE := false
+LOCAL_SRC_FILES := \
+    intel_scu_ipcutil_test.c \
+    mem_dump.c \
+    gen_utils.c \
+    peeknpoke.c \
+    msr.c \
+    i2c.c \
+    port.c
 include $(BUILD_EXECUTABLE)
